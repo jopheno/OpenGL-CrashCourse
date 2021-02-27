7a345d08f33e0ffe6c9b2915d5267554465c1a43
reyhammer <reyhammer@gmail.com>
2021-02-27 14:20:09 -0300

    feat: fade triangle colors using the interpolation mechanism

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -34,20 +34,25 @@ static const char* vShader = "                                  \n\
                                                                 \n\
 layout (location = 0) in vec3 pos;                              \n\
                                                                 \n\
+out vec4 vCol;                                                  \n\
+                                                                \n\
 uniform mat4 model;                                             \n\
                                                                 \n\
 void main() {                                                   \n\
     gl_Position = model * vec4(pos, 1.0);                       \n\
+    vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);                  \n\
 }";
 
 // Fragment Shader
 static const char* fShader = "                                  \n\
 #version 330                                                    \n\
                                                                 \n\
+in vec4 vCol;                                                  \n\
+                                                                \n\
 out vec4 colour;                                                \n\
                                                                 \n\
 void main() {                                                   \n\
-    colour = vec4(1.0, 0.0, 0.0, 1.0);                          \n\
+    colour = vCol;                                              \n\
 }";
 
 
@@ -229,9 +234,9 @@ int main(int argc, char** argv) {
         glUseProgram(shader);
         {
             glm::mat4 model(1.0f);
-            model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-            model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-            model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+            //model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+            //model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+            model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
             
             glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
