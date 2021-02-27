20ca9b3e186ff59ca8f14270a2bc977b2d5ee117
reyhammer <reyhammer@gmail.com>
2021-02-27 14:55:07 -0300

    feat: add a perspective projection matrix

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -14,7 +14,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = M_PI / 180.0f;
 
-GLuint VAO, VBO, IBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -37,9 +37,10 @@ layout (location = 0) in vec3 pos;                              \n\
 out vec4 vCol;                                                  \n\
                                                                 \n\
 uniform mat4 model;                                             \n\
+uniform mat4 projection;                                        \n\
                                                                 \n\
 void main() {                                                   \n\
-    gl_Position = model * vec4(pos, 1.0);                       \n\
+    gl_Position = projection * model * vec4(pos, 1.0);          \n\
     vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);                  \n\
 }";
 
@@ -157,6 +158,7 @@ void CompileShaders() {
     }
     
     uniformModel = glGetUniformLocation(shader, "model");
+    uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main(int argc, char** argv) {
@@ -220,6 +222,8 @@ int main(int argc, char** argv) {
     }
     glBindVertexArray(0);
     
+    glm::mat4 projection = glm::perspective(45.0f, static_cast<GLfloat>(bufferWidth) / static_cast<GLfloat>(bufferHeight), 0.1f, 100.0f);
+    
     // Loop until window closed
     while(!glfwWindowShouldClose(mainWindow)) {
         // Get + handle user input events
@@ -255,11 +259,13 @@ int main(int argc, char** argv) {
         glUseProgram(shader);
         {
             glm::mat4 model(1.0f);
-            //model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-            model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+
+            model = glm::translate(model, glm::vec3(triOffset, 0.0f, -2.5f));
+            //model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
             model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
             
             glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+            glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
             glBindVertexArray(VAO);
             
