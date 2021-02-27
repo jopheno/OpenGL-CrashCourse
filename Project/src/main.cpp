5349f0ea5f122816def6994d479c26e78af2a413
reyhammer <reyhammer@gmail.com>
2021-02-27 13:17:28 -0300

    feat: add uniform variable to move triangle in X coordinate

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 #include <string>
 
 #include <GL/glew.h>
@@ -7,7 +9,12 @@
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxoffset = 0.7f;
+float triIncrement = 0.005f;
 
 // Vertex Shader
 static const char* vShader = "                                  \n\
@@ -15,8 +22,15 @@ static const char* vShader = "                                  \n\
                                                                 \n\
 layout (location = 0) in vec3 pos;                              \n\
                                                                 \n\
+uniform float xMove;                                            \n\
+                                                                \n\
 void main() {                                                   \n\
-    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);   \n\
+    gl_Position = vec4(                                         \n\
+        0.4 * pos.x + xMove,                                    \n\
+        0.4 * pos.y,                                            \n\
+        pos.z,                                                  \n\
+        1.0                                                     \n\
+    );                                                          \n\
 }";
 
 // Fragment Shader
@@ -111,6 +125,8 @@ void CompileShaders() {
         printf("Error validating program: '%s'\n", eLog);
         return;
     }
+    
+    uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main(int argc, char** argv) {
@@ -176,12 +192,24 @@ int main(int argc, char** argv) {
         // Get + handle user input events
         glfwPollEvents();
         
+        if (direction) {
+            triOffset += triIncrement;
+        } else {
+            triOffset -= triIncrement;
+        }
+        
+        if (abs(triOffset) >= triMaxoffset) {
+            direction = !direction;
+        }
+        
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(shader);
         {
+            glUniform1f(uniformXMove, triOffset);
+
             glBindVertexArray(VAO);
             
             {
